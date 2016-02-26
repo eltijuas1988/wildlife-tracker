6e8750c05e44521ec9256ee6bbff0339d1348849
daniel <crimsondex@yahoo.com>
2016-02-26 11:50:08 -0800

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'species/index'
+
+  get 'species/create'
+
+  get 'species/edit'
+
+  get 'species/destroy'
+
+  get 'species/update'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
